b0ef8d5f8d500d7fbea6ee9898f712a82f7c389a
Lodrick <dev.frenchlod@gmail.com>
2024-07-18 22:43:30 +0200

    ✅ to acept cookie

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,32 @@ import { ToastContainer } from "react-toastify";
 import Home from "./Home/Home";
 import { COLORS } from "./styles/styles";
 import "react-toastify/dist/ReactToastify.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Loading from "./Loading/Loading";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import PopFooter from "./Footer/PopFooter";
+import PopUp from "./PopUp/PopUp";
 
 function App() {
   const [loader, setLoader] = useState<boolean>(false);
   const [popFooter, setPopFooter] = useState<string>();
+  const [cookiePop, setCookiePop] = useState<boolean>(false);
+  const checkIfAccept = () => {
+    const nameEQ = "acceptSkillXp" + "=";
+    const ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === " ") c = c.substring(1);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  };
+  useEffect(() => {
+    if (!checkIfAccept()) {
+      setCookiePop(true);
+    }
+  }, []);
   return (
     <StyledApp className="App">
       <Header />
@@ -22,6 +39,7 @@ function App() {
       {popFooter && (
         <PopFooter popFooter={popFooter} setPopFooter={setPopFooter} />
       )}
+      {cookiePop && <PopUp />}
       <Footer setPopFooter={setPopFooter} />
     </StyledApp>
   );
---
+++ src/PopUp/PopUp.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+import { COLORS } from "../styles/styles";
+
+const PopUp = () => {
+  const accept = () => {
+    const date = new Date();
+    date.setTime(date.getTime() + 90 * 24 * 60 * 60 * 1000); // Convertir les jours en millisecondes
+    const expires = "expires=" + date.toUTCString();
+    document.cookie =
+      "acceptSkillXp" + "=" + "okAcceptToSKillXP" + ";" + expires + ";path=/";
+    window.location.reload();
+  };
+  const redirectToGoogle = () => {
+    window.location.href = "https://www.google.com";
+  };
+  return (
+    <StyledPopUp>
+      <div>
+        <p>
+          En naviguant sur SkillXP, vous acceptez nos conditions d'utilisation
+          et notre politique de confidentialité.
+        </p>
+        <button onClick={accept}>Oui</button>
+        <button className="red-btn" onClickCapture={redirectToGoogle}>
+          Non
+        </button>
+      </div>
+    </StyledPopUp>
+  );
+};
+
+export default PopUp;
+const StyledPopUp = styled.div`
+  background: ${COLORS.transparent};
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  position: absolute;
+  z-index: 13;
+  height: 50vh;
+  width: 70%;
+  top: 50%;
+  right: 50%;
+  transform: translate(50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > div {
+    display: flex;
+    flex-direction: column;
+    p {
+      color: ${COLORS.light};
+    }
+    button {
+      margin-top: 10px;
+      width: 20%;
+      border: none;
+      border-radius: 3px;
+      background: ${COLORS.blue};
+      font-size: 1em;
+      padding: 3px;
+      cursor: pointer;
+    }
+    .red-btn {
+      background: ${COLORS.second} !important;
+    }
+  }
+`;
