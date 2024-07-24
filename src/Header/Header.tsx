4bc5e8e762cf0197cdf55aff67c2de170a7618be
Lodrick <dev.frenchlod@gmail.com>
2024-07-24 12:03:49 +0200

    ✅ icon image logo and reponsive

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -7,7 +7,8 @@ import { PropsAvis } from "../Types/Types";
 const Header = ({ setSendAvis }: PropsAvis) => {
   return (
     <StyledHeader>
-      <FaWpexplorer className="icon-header-logo" />
+      {/* <FaWpexplorer className="icon-header-logo" /> */}
+      <img src="./assets/skillXp.png" alt="logo skillxp" />
       <h1>{process.env.REACT_APP_NAME} - IA</h1>
       <button onClick={() => setSendAvis(true)}>Un avis ?</button>
     </StyledHeader>
@@ -19,7 +20,12 @@ const StyledHeader = styled.header`
   /* background: blue; */
   display: flex;
   justify-content: flex-start !important;
+  align-items: center;
   padding-top: 10px;
+  img {
+    display: block;
+    width: 4%;
+  }
   .icon-header-logo {
     font-size: 2em;
     margin-right: 15px;
@@ -29,8 +35,9 @@ const StyledHeader = styled.header`
     color: ${COLORS.second};
   }
   button {
+    height: 5vh;
     padding: 0px 15px;
-    margin-left: 15px;
+    margin-left: 20px;
     border-radius: 10px;
     outline: none;
     border: none;
@@ -38,5 +45,11 @@ const StyledHeader = styled.header`
     color: ${COLORS.light};
     font-weight: 800;
     cursor: pointer;
+  }
+  //width =< 425px
+  @media screen and (max-width: 429px) {
+    img {
+      width: 10%;
+    }
   }
 `;
